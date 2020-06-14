6c5300dfe3bb4831b951e91a7e2db5a8eb38b29a
marzanulhoque <marzanulru17@gmail.com>
2020-06-14 22:23:05 +0600

    MYSQL DATABASE ADDED

--- Inventory_Home/Inventory_Home/settings.py
+++ Inventory_Home/Inventory_Home/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Inventory_Home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventory2020',
+        'USER':'swazan',
+        'PASSWORD' : '1q2w3e4r',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
