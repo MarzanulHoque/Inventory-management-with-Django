b6941f337c3b82c87ea0bc8d9f32ea89bf1cccfd
marzanulhoque <marzanulru17@gmail.com>
2020-06-15 20:52:37 +0600

    Session key addd

--- Inventory_Home/adminInv/models.py
+++ Inventory_Home/adminInv/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class User(models.Model):
     uid = models.CharField(max_length=10)
+    sesskey = models.CharField(max_length=50)
     fullName = models.CharField(max_length=50)
     role = models.CharField(max_length=30)
     password =  models.CharField(max_length=30)
